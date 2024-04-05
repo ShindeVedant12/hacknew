46f50447e0058cba6963ab395cf95ea6e4d14b61
ShindeVedant12 <161154285+ShindeVedant12@users.noreply.github.com>
2024-04-06 00:03:45 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Linear Algebra',
+        productNumber: '001',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Zetilli',
+        productNumber: '002',
+        paymentStatus: 'Reissued',
+        status: 'Accepted'
+    },
+    {
+        productName: 'Kenneth Rosen',
+        productNumber: '003',
+        paymentStatus: 'Available',
+        status: 'Active'
+    },
+]
