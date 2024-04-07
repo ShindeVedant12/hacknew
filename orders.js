37c0cff1b197b1b09fa6f523b9a4739928abb52e
sahilmalawde <161219991+sahilmalawde@users.noreply.github.com>
2024-04-07 12:26:55 +0530

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,20 +1,28 @@
-const Orders = [
-    {
-        productName: 'Linear Algebra',
-        productNumber: '001',
-        paymentStatus: 'Due',
-        status: 'Pending'
-    },
-    {
-        productName: 'Zetilli',
-        productNumber: '002',
-        paymentStatus: 'Reissued',
-        status: 'Accepted'
-    },
-    {
-        productName: 'Kenneth Rosen',
-        productNumber: '003',
-        paymentStatus: 'Available',
-        status: 'Active'
-    },
-]
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getDatabase,ref,child,get,update} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcicHvJR8SsPppjverjAVmWUbxz4fWRrU",
+  authDomain: "final-venue-booking.firebaseapp.com",
+  projectId: "final-venue-booking",
+  storageBucket: "final-venue-booking.appspot.com",
+  messagingSenderId: "130315557712",
+  appId: "1:130315557712:web:10ee9fc8cea3aeb1d7ea55"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+  get(child(ref(database),'books/'))
+  .then((snapshot)=>{
+    var Orders = snapshot.val();
+    return Orders
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  alert(errorMessage);})
+
