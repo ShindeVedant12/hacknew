08da21d8815d6bcada1fb4327ada77c3952eb1ca
sahilmalawde <161219991+sahilmalawde@users.noreply.github.com>
2024-04-08 22:46:04 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getDatabase,ref,child,get,update} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcicHvJR8SsPppjverjAVmWUbxz4fWRrU",
+  authDomain: "final-venue-booking.firebaseapp.com",
+  projectId: "final-venue-booking",
+  storageBucket: "final-venue-booking.appspot.com",
+  messagingSenderId: "130315557712",
+  appId: "1:130315557712:web:10ee9fc8cea3aeb1d7ea55"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase();
+const user = auth.currentUser;
+const userId = user.uid;
+const userRef = ref(database, 'users/' + userId);
+
+get(userRef).then((snapshot) => {
+    const userData = snapshot.val();
+    const username = userData.username;
+    const useremail = userData.email;
+    const userstatus = userData.status;
+
+    document.getElementById('name').innerHTML = username;
+    document.getElementById('email').innerHTML = useremail;
+    document.getElementById('name').innerHTML = userstatus;
+
+    return username;
+})
+
+
+
+
 
