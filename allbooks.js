09daff0a01e5d1e447b2e95c6ee652602fbf6b2d
sahilmalawde <161219991+sahilmalawde@users.noreply.github.com>
2024-04-09 23:44:03 +0530

    Create allbooks.js

---
+++ allbooks.js
@@ -0,0 +1,66 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getDatabase,ref,child,get,update} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcicHvJR8SsPppjverjAVmWUbxz4fWRrU",
+  authDomain: "final-venue-booking.firebaseapp.com",
+  projectId: "final-venue-booking",
+  storageBucket: "final-venue-booking.appspot.com",
+  messagingSenderId: "130315557712",
+  appId: "1:130315557712:web:10ee9fc8cea3aeb1d7ea55"
+};
+function convertMillisecondsToDate(milliseconds) {
+    // Create a new Date object with milliseconds
+    const date = new Date(milliseconds);
+
+    // Get the various components of the date
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Adding 1 because getMonth() returns zero-based index
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    // Format the date as YYYY-MM-DD HH:MM:SS
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return formattedDate;
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+  get(child(ref(database),'books/'))
+  .then((snapshot)=>{
+    var Orders = snapshot.val();
+    var order;
+    var cnt = 1;
+    for (order in Orders){
+        order = Orders[cnt]
+        const tr = document.createElement('tr');
+        var date = convertMillisecondsToDate(order.doi)
+        var date2 = convertMillisecondsToDate(order.doi + 86400000 * 7)
+        
+        const trContent = `
+            <td>${order.id}</td>
+            <td>${order.name}</td>
+            <td class="${order.status === 'Due' ? 'danger' : order.status === 'available' ? 'success' : 'primary'}">${order.status}</td>
+            <td><button>issue</button></td>
+            
+        `;
+        tr.innerHTML = trContent;
+        document.querySelector('table tbody').appendChild(tr);
+        cnt++;
+    };
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  alert(errorMessage);})
+
+  
